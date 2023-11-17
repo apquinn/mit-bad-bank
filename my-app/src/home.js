d884ca727bba656f290dffa95e1b4fd8f64a0670
Drew Quinn <apquinn@gmail.com>
2023-11-17 16:29:46 -0500

    test stuck on filling iput fields

--- my-app/src/home.js
+++ my-app/src/home.js
@@ -8,7 +8,7 @@ export default function Home() {
         header="BadBank Landing Module"
         title="Welcome to the bank"
         text="You can move around using the navigation bar."
-        body={<img src="bank.png" className="img-fluid" alt="bank" />}
+        body={<img src="./bank.png" className="img-fluid" alt="bank" />}
       />
     </>
   );
--- my-app/src/tests/login.test.jsx
+++ my-app/src/tests/login.test.jsx
@@ -1,8 +1,22 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import { App } from "../app";
+import React from "react";
+import "@testing-library/jest-dom";
 
-test("User login component should render", () => {
-  const { getByText, getByLabel } = render(<App />);
+test("User login component should render", async () => {
+  const { getByText, getByLabelText } = render(<App />);
 
-  console.log(getByText);
+  getByText("Welcome to the bank");
+
+  await fireEvent.click(screen.getByText("Login"));
+  expect(screen.getByText("Login")).toBeInTheDocument();
+
+  fireEvent.change(getByLabelText("Email"), {
+    target: { value: "abel@mit.edu" },
+  });
+  fireEvent.change(getByLabelText("Password"), {
+    target: { value: "secret" },
+  });
+
+  fireEvent.click(getByText("Login"));
 });
